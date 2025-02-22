bf05e941796479fd897eff3b0be5bc382dae78a2
ariaramadhani <ariaramadhani72@gmail.com>
2025-02-22 17:10:10 +0700

    fix ui login form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 // import RestaurantDetail from "./components/RestaurantDetail/RestaurantDetail";
 // import CheckoutPage from "./components/CheckoutPage/CheckoutPage";
 import "./globals.css";
-import axios from 'axios'
+// import axios from 'axios'
 import { useState, type FormEvent, type ChangeEvent } from "react"
 // import { Button } from "@/components/ui/button"
 // import { Input } from "@/components/ui/input"
@@ -46,28 +46,30 @@ const LoginForm = () => {
   }
 
   return (
-    <div>
-        <div className="place-content-center">
+    <div className='bg-slate-200 min-h-screen flex items-center justify-center'>
+        <div className="justify-center items-center bg-white p-6 rounded shadow-md">
             <form onSubmit={handleSubmit} className="space-y-4">
-                <div className="space-y-2">
+                <div className="space-y-2 flex flex-col">
                     <label htmlFor="email">Email</label>
                         <input
                         id="email"
                         name="email"
                         type="email"
                         placeholder="Enter your email"
+                        className='p-2 rounded-lg'
                         value={formData.email}
                         onChange={handleChange}
                         required
                     />
                 </div>    
-                <div className="space-y-2">
+                <div className="space-y-2 flex flex-col">
                     <label htmlFor="password">Password</label>
                         <input
                         id="password"
                         name="password"
                         type="password"
                         placeholder="Enter your password"
+                        className='p-2 rounded-lg'
                         value={formData.password}
                         onChange={handleChange}
                         required
